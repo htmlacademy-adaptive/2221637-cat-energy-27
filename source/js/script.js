5e8e5aa5454a7c046f08ef8c7ff6f70408e8e9b5
munkimu <naduh@list.ru>
2023-03-04 00:00:28 +0300

    1

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,17 @@
 
-let navAll = document.querySelector('.site-navigation');
+let navAll = document.querySelector('.navigation');
 
-let navList = navAll.querySelector('.site-navigation__list')
-let navToggle = navAll.querySelector('.site-navigation__toggle');
+let navList = navAll.querySelector('.navigation__list')
+let navToggle = navAll.querySelector('.navigation__toggle');
 
 // let mapFrame = document.querySelector('.map__frame');
 // let mapImage = document.querySelector('.map__image');
 
-navAll.classList.remove('site-navigation--no-js');
+navAll.classList.remove('navigation--no-js');
 
 navToggle.onclick = function() {
-  navToggle.classList.toggle('site-navigation__toggle--is-open');
-  navList.classList.toggle('site-navigation__list--visible');
+  navToggle.classList.toggle('navigation__toggle--is-open');
+  navList.classList.toggle('navigation__list--visible');
 }
 
 // mapFrame.classList.remove('map__frame--no-js');
